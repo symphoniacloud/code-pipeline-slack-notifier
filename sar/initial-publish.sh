d48ca94930d280d5758a4c6a7ebf05c506dcdc24
Mike Roberts <mike@symphonia.io>
2018-03-14 13:35:00 -0400

    Prepping for bug report

--- sar/initial-publish.sh
+++ sar/initial-publish.sh
@@ -35,16 +35,14 @@ fi
 # An error occurred (BadRequestException) when calling the CreateApplication operation: Resource with name [template] is invalid. Expected input as dictionary, instead got <type 'str'>
 # Mikes-MacBook-Pro-2:code-pipeline-slack-notifier mike$ cat target/packaged-template.yaml
 
-# ./upload-package.sh
-
-# aws serverlessrepo create-application \
-# --author "Symphonia" \
-# --description "Post notifications to Slack when Code Pipeline Events occur" \
-# --labels "CodePipeline" "Slack" \
-# --license-body LICENSE \
-# --name "CodePipelineSlackNotifier" \
-# --readme-body README-SAR.md \
-# --semantic-version "1.0.0" \
-# --spdx-license-id "Apache-2.0" \
-# --template-body target/packaged-template.yaml \
-# --source-code-url https://github.com/symphoniacloud/code-pipeline-slack-notifier
+aws serverlessrepo create-application \
+--author "Symphonia" \
+--description "Post notifications to Slack when Code Pipeline Events occur" \
+--labels "CodePipeline" "Slack" \
+--license-body LICENSE \
+--name "CodePipelineSlackNotifier" \
+--readme-body sar/README-SAR.md \
+--semantic-version "1.0.0" \
+--spdx-license-id "Apache-2.0" \
+--template-body target/packaged-template.yaml \
+--source-code-url https://github.com/symphoniacloud/code-pipeline-slack-notifier
